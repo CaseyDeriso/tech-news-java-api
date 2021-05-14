b385a7abcc8ad3ecf6174d1df184f70cdff1817c
Casey Deriso <caseyderiso@gmail.com>
2021-05-14 11:34:00 -0500

    created Entity Models for User, Post, Comment, and Vote.

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int number = 10;
+        double otherNumber = 10.1;
+        String word = "wazzaaa";
+
+        double sum = number + otherNumber;
+        String weirdSum = word + number;
+        System.out.println(sum + " " + weirdSum);
+    }
+}
